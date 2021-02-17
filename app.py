a5d4e9e1abebde011056cb7c7d7b71bcc7691c6a
EustaceMwai <43333927+EustaceMwai@users.noreply.github.com>
2021-02-17 22:51:20 +0300

    question

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, session, redirect, url_for
 from database import get_db, init_db_command
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 
 @app.teardown_appcontext
@@ -11,13 +13,28 @@ def close_db(error):
         g.sqlite_db.close()
 
 
+def get_current_user():
+    user_result = None
+
+    if 'user' in session:
+        user = session['user']
+
+        db = get_db()
+        user_cur = db.execute('select id, name, password, expert, admin from users where name = ?', [user])
+        user_result = user_cur.fetchone()
+    return user_result
+
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    user = get_current_user()
+
+    return render_template('home.html', user=user)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    user = get_current_user()
     if request.method == 'POST':
         db = get_db()
         hashed_password = generate_password_hash(request.form['password'], method='sha256')
@@ -25,13 +42,16 @@ def register():
                    [request.form['name'], hashed_password, '0', '0'])
         db.commit()
 
-        return '<h1>Eustero user created</h1>'
+        session['user'] = request.form['name']
 
-    return render_template('register.html')
+        return redirect(url_for('index'))
+
+    return render_template('register.html', user=user)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    user = get_current_user()
     if request.method == 'POST':
         db = get_db()
 
@@ -42,36 +62,77 @@ def login():
         user_result = user_cur.fetchone()
 
         if check_password_hash(user_result['password'], password):
-            return '<h1> The password is correct! </h1>'
+            session['user'] = user_result['name']
+            return redirect(url_for('index'))
         else:
             return '<h1> The password is incorrect! </h1>'
 
-    return render_template('login.html')
+    return render_template('login.html', user=user)
 
 
 @app.route('/question')
 def question():
-    return render_template('question.html')
+    user = get_current_user()
+    return render_template('question.html', user=user)
 
 
 @app.route('/answer')
 def answer():
-    return render_template('answer.html')
+    user = get_current_user()
+    return render_template('answer.html', user=user)
 
 
-@app.route('/ask')
+@app.route('/ask', methods=['GET', 'POST'])
 def ask():
-    return render_template('ask.html')
+    user = get_current_user()
+    db = get_db()
+
+    if request.method == 'POST':
+        db.execute('insert into questions (question_text, asked_by_id, expert_id) values(?, ?, ?)', [request.form['question'], user['id'], request.form['expert']])
+        db.commit()
+
+        return redirect(url_for('index'))
+
+    user = get_current_user()
+    db = get_db()
+    expert_cur = db.execute('select id, name from users where expert = 1')
+    expert_results = expert_cur.fetchall()
+
+    return render_template('ask.html', user=user, experts=expert_results)
 
 
 @app.route('/unanswered')
 def unanswered():
-    return render_template('unanswered.html')
+    user = get_current_user()
+    db = get_db()
+    questions_cur = db.execute('select id, question_text, asked_by_id')
+    return render_template('unanswered.html', user=user, questions=questions)
 
 
 @app.route('/users')
 def users():
-    return render_template('users.html')
+    user = get_current_user()
+
+    db = get_db()
+    users_cur = db.execute('select id, name, expert, admin from users')
+    users_result = users_cur.fetchall()
+
+    return render_template('users.html', user=user, users=users_result)
+
+
+@app.route('/promote/<user_id>')
+def promote(user_id):
+    db = get_db()
+    db.execute('update users set expert = 1 where id = ?', [user_id])
+    db.commit()
+
+    return redirect(url_for('users'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
